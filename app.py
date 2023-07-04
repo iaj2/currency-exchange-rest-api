6f96899c34b70072d9e1a26ffb16a62e5c15ea8a
iaj2 <isaacjames124@yahoo.ca>
2023-07-03 23:53:05 -0700

    use function not class for scrape and use in app

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, jsonify
+from scraper import currency_exchange_scraper
 
 app = Flask(__name__)
 
-my_dictionary = {}
+curr_exchange_rate_dic = currency_exchange_scraper.get_exchange_rates_dic()
 
 # Route to handle GET request for the conversion dictionary
 @app.route('/exchange-rates', methods=['GET'])
 def get_dictionary():
-    return jsonify(my_dictionary)
+    return jsonify(curr_exchange_rate_dic)
 
 if __name__ == '__main__':
     app.run()
